0e37355339ef831621bcf10da1d0deee9a795d52
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-05-23 19:05:38 -0500

    update error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers} = require('./schemas');
+const { typeDefs, resolvers} = require('./schema');
 const {authMiddleware} = require('./utils/auth');
 
 
